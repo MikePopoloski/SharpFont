e12ce5d75e8fc9cd37eadb7f3348e7b548b0514c
Mike Popoloski <mike@popoloski.com>
2015-06-13 19:01:44 -0400

    Adding text buffer classes.

---
+++ GpuExample/TextBuffer.cs
@@ -0,0 +1,16 @@
+﻿using SlimFont;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GpuExample {
+    class TextBuffer {
+        public TextBuffer () {
+        }
+
+        public void Append (Typeface typeface, string text) {
+        }
+    }
+}
